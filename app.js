17205b40b88358da265c3aa582d2e605bb3bd5ca
theDTS1 <orel1one@gmail.com>
2022-06-12 17:03:04 +0300

    Added notes.js and removed utils.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-/* Importing the function from the utils.js file and then calling it with the arguments 4 and -2. */
-const add = require("./utils.js");
+const getNotes = require("./notes.js");
+// const add = require("./utils.js");
 
-const sum = add(4, -2);
+// const sum = add(4, -2);
 
-console.log(sum);
+// console.log(sum);
+
+const message = getNotes();
+console.log("====================================");
+console.log(message);
+console.log("====================================");
--- utils.js
+++
@@ -1,5 +0,0 @@
-console.log("utils.js");
-
-const name = "MikeTest";
-
-module.exports = name;
