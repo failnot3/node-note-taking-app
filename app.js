5a69fb12ef41999e3a6e718fce73bdbad8eabc46
theDTS1 <orel1one@gmail.com>
2022-06-12 16:15:45 +0300

    Changes

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const fs = require("fs");
+require("./utils.js");
 
-// fs.writeFileSync("notes.txt", "This file was created by NodeJS");
+// const fs = require("fs");
 
-fs.appendFileSync("notes.txt", "\n Appended a new line!");
+// // fs.writeFileSync("notes.txt", "This file was created by NodeJS");
+
+// fs.appendFileSync("notes.txt", "\n Appended a new line!");
+
+const name = "Test123";
+console.log(name);
