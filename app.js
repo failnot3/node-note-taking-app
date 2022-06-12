1fc7db2d06270c96a29740b1e08bb7379dbb49a2
theDTS1 <orel1one@gmail.com>
2022-06-12 18:02:41 +0300

    Added chalk

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const chalk = require("chalk");
 /* Importing the validator module and the getNotes module. */
-const validator = require("validator");
+// const validator = require("validator");
 
 const getNotes = require("./notes.js");
 /* Importing the getNotes module and then logging the message to the console. */
@@ -10,3 +11,6 @@ console.log("====================================");
 
 /* Using the validator module to check if the string is a URL. */
 // console.log(validator.isURL("https://google.com"));
+
+/* Using the chalk module to print the string "Hello world!" in blue. */
+console.log(chalk.blue("Hello world!"));
