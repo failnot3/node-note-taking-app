ab5ba57ad8a587f2f8f2379fd1d2c4a81e9518b4
theDTS1 <orel1one@gmail.com>
2022-06-12 17:17:40 +0300

    Changes to app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-/* Importing the notes.js file and then calling the function getNotes() - then printing it to the terminal*/
-const getNotes = require("./notes.js");
+/* Importing the validator module and the getNotes module. */
+const validator = require("validator");
 
+const getNotes = require("./notes.js");
+/* Importing the getNotes module and then logging the message to the console. */
 const message = getNotes();
 console.log("====================================");
 console.log(message);
 console.log("====================================");
+
+/* Using the validator module to check if the string is a valid email. */
+console.log(validator.isEmail("peter@example.com"));
